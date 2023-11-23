6cc20a625b5ffb1105b9344ecec29258a52ae897
Queereell <thelegendaryyouth@gmail.com>
2023-11-23 15:23:32 +0100

    fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,42 +1,23 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./scss/Main.scss";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Outlet,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Index from "./Routes/Index";
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  /* DETTA ÄR BARA ROUTER, ÄNDRA INGET HÄR */
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div id="container">
+      <Router>
+        <Routes>
+          <Route path="/">
+            <Route>
+              <Route index element={<Index />} />
+              <Route path="Other" element={<Index />} />
+            </Route>
+          </Route>
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
