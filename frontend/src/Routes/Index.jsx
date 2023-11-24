240e150fc8ddca375726cdc192e674b33367685d
WarMolee <142778225+WarMolee@users.noreply.github.com>
2023-11-24 15:07:33 +0100

    started on header

--- frontend/src/Routes/Index.jsx
+++ frontend/src/Routes/Index.jsx
@@ -5,23 +5,19 @@ const Index = () => {
   /* START SIDA */
   return (
     <>
-    <Header />
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer"></a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => fetch("/randomize")}></button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+      <Header />
+      <div id="main">
+        <h1>Vite + React</h1>
+        <div className="card">
+          <button onClick={() => fetch("/randomize")}></button>
+          <p>
+            Edit <code>src/App.jsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 };
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,4 +1,18 @@
+import menuIcon from "../assets/menu.svg";
+
 const Header = () => {
-  return <><p>Header</p></>;
+  return (
+    <>
+      <div id="header">
+        <div id="leftHeader">
+          <img src={menuIcon} alt="Menu" />
+        </div>
+        <div id="rightHeader">
+          <h3>Edit Class</h3>
+          <h3>Randomize Group</h3>
+        </div>
+      </div>
+    </>
+  );
 };
 export default Header;
