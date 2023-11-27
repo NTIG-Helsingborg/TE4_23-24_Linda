fd731fa866d21f775a9cab4d0ee0d6c169523b5b
WarMolee <142778225+WarMolee@users.noreply.github.com>
2023-11-27 10:57:31 +0100

    added group display component

--- frontend/src/Routes/Index.jsx
+++ frontend/src/Routes/Index.jsx
@@ -2,29 +2,15 @@ import React, { useState } from "react";
 
 import reactLogo from "../assets/react.svg";
 import Header from "../components/Header.jsx";
+import GroupDisplay from "../components/GroupDisplay";
 
 const Index = () => {
   /* START SIDA */
   return (
     <>
-        <Header />
+      <Header />
       <div id="main">
-        <h1>Vite + React</h1>
-        <div className="card">
-            <button
-            onClick={() =>
-              fetch("/randomize", {
-              method: "POST",
-              body: JSON.stringify({}),
-            })
-                .then((response) => response.json())
-                .then((json) => console.log(json))
-            }
-          ></button>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test HMR
-          </p>
-        </div>
+        <GroupDisplay />
         <p className="read-the-docs">
           Click on the Vite and React logos to learn more
         </p>
