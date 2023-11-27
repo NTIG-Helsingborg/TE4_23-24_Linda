7ab2b73b2d5920c585ac5ba425bbe80fb023311f
WarMolee <142778225+WarMolee@users.noreply.github.com>
2023-11-27 10:43:21 +0100

    added simple classelect

--- frontend/src/Routes/Index.jsx
+++ frontend/src/Routes/Index.jsx
@@ -1,3 +1,5 @@
+import React, { useState } from "react";
+
 import reactLogo from "../assets/react.svg";
 import Header from "../components/Header.jsx";
 
@@ -5,23 +7,25 @@ const Index = () => {
   /* START SIDA */
   return (
     <>
-    <Header />
-      <h1>Vite + React</h1>
-      <div className="card">
-      <button
-          onClick={() =>
-            fetch("/randomize?groupCount=2")
-              .then((response) => response.json())
-              .then((json) => console.log(json))
-          }
-        ></button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+      <Header />
+      <div id="main">
+        <h1>Vite + React</h1>
+        <div className="card">
+          <button
+            onClick={() =>
+              fetch("/randomize?groupCount=2")
+                .then((response) => response.json())
+                .then((json) => console.log(json))
+            }
+          ></button>
+          <p>
+            Edit <code>src/App.jsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 };
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,17 +1,23 @@
+import React, { useState } from "react";
 import menuIcon from "../assets/menu.svg";
+import ClassSelect from "./ClassSelect.jsx";
 
 const Header = () => {
+  const [toggle, setToggle] = useState(false);
   return (
     <>
       <div id="header">
         <div id="leftHeader">
-          <img src={menuIcon} alt="Menu" />
+          <button onClick={() => setToggle(!toggle)}>
+            <img src={menuIcon} alt="Menu" />
+          </button>
         </div>
         <div id="rightHeader">
           <h3>Edit Class</h3>
           <h3>Randomize Group</h3>
         </div>
       </div>
+      {toggle && <ClassSelect />}
     </>
   );
 };
