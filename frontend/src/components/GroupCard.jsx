47aa3b4152406f6d2b769b7eb22672de974c7cda
WarMolee <142778225+WarMolee@users.noreply.github.com>
2023-11-28 10:17:54 +0100

    group count is now customizible

--- frontend/src/components/GroupCard.jsx
+++ frontend/src/components/GroupCard.jsx
@@ -1,40 +1,43 @@
 import react from "../assets/NtiPush.jpg";
 
-const GroupCard = ({}) => {
-  const groups = [
-    { group: 1 },
-    { group: 2 },
-    { group: 3 },
-    { group: 4 },
-    { group: 5 },
-    { group: 6 },
-  ];
-  const students = [
-    { name: "John Henric", image: "image1.jpg" },
-    { name: "Stina Fishing", image: "image2.jpg" },
-    { name: "Marko Urpers", image: "image3.jpg" },
-    { name: "Bob Bobbington The third", image: "image4.jpg" },
-  ];
+const GroupCard = ({ groupCount, groupAmount}) => {
+    //var groupAmount = 2;
 
-  return (
-    <>
-      {groups.map((group, index) => (
-        <div key={index} id="card">
-          <p id="title">Group {group.group}</p>
-          <div id="group">
-            {students.map((student, studentIndex) => (
-              <div
-                className={`student`}
-                key={studentIndex}
-              >
-                <img src={react} alt={""} className="img" />
-                <div className="name">{student.name}</div>
-              </div>
+
+    const groups = [
+        { group: 1 },
+        { group: 2 },
+        { group: 3 },
+        { group: 4 },
+        { group: 5 },
+        { group: 6 },
+    ];
+
+    const students = [
+        { name: "John Henric", image: "image1.jpg" },
+        { name: "Stina Fishing", image: "image2.jpg" },
+        { name: "Marko Urpers", image: "image3.jpg" },
+        { name: "Bob Bobbington The third", image: "image4.jpg" },
+    ];
+    return (
+        <>
+            {groups.slice(0, groupCount).map((group, index) => (
+                <div id={`group${groupAmount}`} key={index}>
+                    <div id="card">
+                        <p id="title">Group {group.group}</p>
+                        <div className="group">
+                            {students.map((student, studentIndex) => (
+                                <div className={`student`} key={studentIndex}>
+                                    <img src={react} alt={""} className="img" />
+                                    <div className="name">{student.name}</div>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
             ))}
-          </div>
-        </div>
-      ))}
-    </>
-  );
+        </>
+    );
 };
+
 export default GroupCard;
